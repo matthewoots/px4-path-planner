be8817af7b95b32fde6374e677ce4991f37e248b
matthewoots <matthewoots@gmail.com>
2022-03-04 17:27:03 +0800

    Added radxa support and launch, and added bash files to launch for testing

---
+++ launch/run_single_node.bash
@@ -0,0 +1,58 @@
+RED='\033[0;31m' # ${RED}
+NC='\033[0m' # ${NC} no color
+GREEN='\033[0;32m' # ${GREEN}
+BLUE='\033[0;34m' # ${BLUE}
+
+export LC_ALL="en_US.UTF-8"
+
+echo "${BLUE}The system is booting...${NC}"
+delay_05=0.5
+delay_1=1
+delay_2=2
+delay_5=5
+
+sleep ${delay_2}
+echo "${GREEN}Sending Takeoff${NC}"
+rostopic pub /S0/global_planner_mission std_msgs/Float32MultiArray "layout: 
+  dim:
+  - label: ''
+    size: 0
+    stride: 0
+  data_offset: 0
+data: [1.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0]" -1
+
+sleep ${delay_5}
+echo "${GREEN}Delay 5${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 10${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 15${NC}"
+sleep ${delay_5}
+
+echo "${GREEN}Sending Bspline mode and mission for S0${NC}"
+rostopic pub /S0/global_planner_mission std_msgs/Float32MultiArray "layout: 
+  dim:
+  - label: ''
+    size: 0
+    stride: 0
+  data_offset: 0
+data: [3.0, 3.0, 14.0, 0.0, 1.5, 0.0, 0.0, 0.0, 0.0]" -1 
+
+sleep ${delay_5}
+echo "${GREEN}Delay 5${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 10${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 15${NC}"
+sleep ${delay_5}
+# echo "${GREEN}Delay 20${NC}"
+# sleep ${delay_5}
+
+echo "${GREEN}Sending Bspline mode and mission for S0${NC}"
+rostopic pub /S0/global_planner_mission std_msgs/Float32MultiArray "layout: 
+  dim:
+  - label: ''
+    size: 0
+    stride: 0
+  data_offset: 0
+data: [3.0, 3.0, 25.0, 0.0, 1.5, 999.0, 999.0, 999.0, 999.0]" -1 
---
+++ launch/run_single_takeoff_land.bash
@@ -0,0 +1,44 @@
+RED='\033[0;31m' # ${RED}
+NC='\033[0m' # ${NC} no color
+GREEN='\033[0;32m' # ${GREEN}
+BLUE='\033[0;34m' # ${BLUE}
+
+export LC_ALL="en_US.UTF-8"
+
+echo "${BLUE}The system is booting...${NC}"
+delay_05=0.5
+delay_1=1
+delay_2=2
+delay_5=5
+
+
+sleep ${delay_2}
+echo "${GREEN}Sending Takeoff${NC}"
+rostopic pub /${UAV_GROUPNAME}/global_planner_mission std_msgs/Float32MultiArray "layout: 
+  dim:
+  - label: ''
+    size: 0
+    stride: 0
+  data_offset: 0
+data: [1.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0]" -1
+
+sleep ${delay_5}
+echo "${GREEN}Delay 5${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 10${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 15${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 20${NC}"
+sleep ${delay_5}
+echo "${GREEN}Delay 25${NC}"
+sleep ${delay_5}
+
+echo "${GREEN}Sending land${NC}"
+rostopic pub /${UAV_GROUPNAME}/global_planner_mission std_msgs/Float32MultiArray "layout: 
+  dim:
+  - label: ''
+    size: 0
+    stride: 0
+  data_offset: 0
+data: [5.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0, 999.0]" -1 
